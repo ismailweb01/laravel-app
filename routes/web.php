3591dfa365336a17b3d6a30f8de4567aa44b1036
iselmaslout <ismaildevox@gmail.com>
2026-08-30 15:32:22 +0100

    Add application health endpoint

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/health', function () {
+    return response()->json([
+        'status' => 'ok',
+        'app' => config('app.name'),
+        'environment' => app()->environment(),
+    ]);
 });
