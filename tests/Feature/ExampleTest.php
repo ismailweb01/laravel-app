a52d26350e4a597a7e57d98a00bfef4a58b75c09
iselmaslout <ismaildevox@gmail.com>
2026-08-30 15:16:38 +0100

    Restore passing CI test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,12 +2,18 @@
 
 namespace Tests\Feature;
 
+// use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    public function test_ci_blocks_bad_code(): void
+    /**
+     * A basic test example.
+     */
+    public function test_the_application_returns_a_successful_response(): void
     {
-        $this->assertTrue(false);
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
     }
 }
